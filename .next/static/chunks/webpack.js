56cb38ba8724fc5d23d9217ed4dc3d432e858fc4
Gustavo Souza <gustavosouzacco@gmail.com>
2021-02-25 09:38:03 -0300

    Notificaçao para iniciar novo desafio

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "5e2cb8cc44222a739e43";
+/******/ 	var hotCurrentHash = "cce056db7a5a4e8a6ac9";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
